467ed44af7de3788da5a0aa3e156067fc1b89400
KanishKumar-M <231001083@rajalakshmi.edu.in>
2024-06-13 13:46:34 +0530

    Create 8.05.py

---
+++ Week08/8.05.py
@@ -0,0 +1,22 @@
+Write a program to eliminate the common elements in the given 2 arrays and print only the non-repeating elements and the total number of such non-repeating elements.
+Input Format:
+The first line contains space-separated values, denoting the size of the two arrays in integer format respectively.
+The next two lines contain the space-separated integer arrays to be compared.
+
+Sample Input:
+5 4
+1 2 8 6 5
+2 6 8 10
+Sample Output:
+1 5 10
+3
+Sample  Input: 
+5 5
+1 2 3 4 5
+1 2 3 4 5
+Sample Output:
+NO SUCH ELEMENTS
+
+
+
+For example:
